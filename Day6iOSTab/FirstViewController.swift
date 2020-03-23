1e48dc9a43bb0e4c96a5978abe2437abc5c111f7
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-23 17:40:48 -0400

    Initial Commit

---
+++ Day6iOSTab/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Day6iOSTab
+//
+//  Created by Nikita Sandhu on 2020-03-23.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day6iOSTab/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Day6iOSTab
+//
+//  Created by Nikita Sandhu on 2020-03-23.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
